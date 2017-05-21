4384798dd96edc02ff7ca0e1e1b8e219b54240b2
smnnsmol <nikolai.smolnikow@tu-dortmund.de>
2017-05-21 21:00:04 +0200

    First Commit

---
+++ akinatorAPI.py
@@ -0,0 +1,57 @@
+import urllib.parse
+import requests
+import json
+import time
+import random
+
+class Apinator:
+    def __init__(self):
+        self.base_url = 'http://api-de1.akinator.com/ws/'
+        self.url = ''
+        self.session = ''
+        self.json_data = ''
+        self.signature = ''
+        self.step = 0
+        self.answerId=''
+        self.question = ''
+        self.characterFound = False
+        self.progression_threshold = 97
+
+    def new_session(self,name):
+        self.url = self.base_url + 'new_session?partner=1&' + urllib.parse.urlencode({'player': name})
+        self.json_data = requests.get(self.url).json()
+        #self.json_data = json.dumps(self.json_data)
+        self.session = self.json_data['parameters']['identification']['session']
+        self.signature = self.json_data['parameters']['identification']['signature']
+        self.question = self.json_data['parameters']['step_information']['question']
+        print(self.url)
+
+
+    def sendAnswer(self, answerId):
+        self.answerId = answerId
+        self.url = self.base_url + 'answer?session=' + self.session + '&signature=' + self.signature + '&step=' + str(self.step) + '&answer=' + self.answerId
+        self.json_data = requests.get(self.url).json()
+        if (self.json_data['completion'] == 'OK'):
+            self.progression = float(self.json_data['parameters']['progression'])
+            if self.progression > self.progression_threshold:
+                self.characterFound = True
+            self.question = self.json_data['parameters']['question']
+            self.step += 1
+
+    def getCharacterGuess(self):
+        self.url = self.base_url + 'list?session=' + self.session + '&signature=' + self.signature \
+                   + '&step=' + str(self.step) + '&size=2&max_pic_width=246&max_pic_height=294&pref_photos=OK-FR&mode_question=0'
+        self.json_data = requests.get(self.url).json()
+        self.character = self.json_data['parameters']['elements'][0]['element']['name']
+        print(self.character)
+
+    def getQuestion(self):
+        print(self.question)
+
+    def isCharacterFound(self):
+        return self.characterFound
+
+    def characterGuessWrong(self):
+        self.characterFound = False
+        if self.progression_threshold < 100:
+            self.progression_threshold += 1
---
+++ akinatorAPITest.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+import akinatorAPI
+
+api = akinatorAPI.Apinator()
+api.new_session('dawn')
+api.getQuestion()
+
+while not api.isCharacterFound():
+    number = random.randint(0,4)
+    api.sendAnswer(str(number))
+    api.getQuestion()
+
+api.getCharacterGuess()
+time.sleep(2)
+api.characterGuessWrong()
+while not api.isCharacterFound():
+    api.sendAnswer('0')
+    api.getQuestion()
+
+api.getCharacterGuess()
